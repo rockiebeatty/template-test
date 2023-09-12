113bf669ebe8e75d32b65e3eefe6a2965399cee1
Rockie Beatty <rockiebeatty@rockies-mbp.lan>
2023-09-12 02:15:43 -0700

    tweaking to coloring of new quote button

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -3,7 +3,7 @@ const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
-const newQuoteBtn = document.getElementById('new-quote');
+const newQuoteBtn = document.getElementById('new-quote-btn');
 const loader = document.getElementById('loader');
 
 
